001a3d5524e0f0359a98da78fcf5a397f5f73ec1
JuranWw <166465470+JuranWw@users.noreply.github.com>
2024-07-25 10:37:24 +1000

    Create template MyName.vue

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+       <!-- FIT5032 <br> Internet application development <br> -->
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <!-- <a href="https://learning.monash.edu/course/view.php?id=19968" target="_blank" rel="noopener">Moodle</a> &  -->
+      <!-- <a href="https://handbook.monash.edu/2024/units/FIT5032" target="_blank" rel="noopener">Handbook</a>. -->
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
